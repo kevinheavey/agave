824345e8d86b91ba0657b9cf897b1550f5df7db4
kevinheavey <heaveykevin2@gmail.com>
2024-04-22 02:33:51 +0400

    remove obsolete comment

--- curve25519/src/lib.rs
+++ curve25519/src/lib.rs
@@ -1,7 +1,4 @@
 //! Syscall operations for curve25519
-//!
-//! This module lives inside the zk-token-sdk for now, but should move to a general location since
-//! it is independent of zk-tokens.
 
 pub mod curve_syscall_traits;
 pub mod edwards;
