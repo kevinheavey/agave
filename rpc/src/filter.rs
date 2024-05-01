bef789bb9f1608f4ff786ad0fb7253b2b7a382ba
kevinheavey <heaveykevin2@gmail.com>
2024-05-01 18:52:29 +0400

    fix: another place where inline token_2022 is needed

--- rpc/src/filter.rs
+++ rpc/src/filter.rs
@@ -1,5 +1,5 @@
 use {
-    solana_inline_spl::token::{Account, GenericTokenAccount},
+    solana_inline_spl::{token::GenericTokenAccount, token_2022::Account},
     solana_rpc_client_api::filter::RpcFilterType,
     solana_sdk::account::{AccountSharedData, ReadableAccount},
 };
