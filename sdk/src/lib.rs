5b3f18c7411a68709c1530570d86fad56f821e08
kevinheavey <heaveykevin2@gmail.com>
2024-06-12 18:23:09 +0400

    move sanitize.rs out of solana-program into its own crate

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -50,7 +50,7 @@ pub use solana_program::{
     decode_error, ed25519_program, epoch_rewards, epoch_schedule, fee_calculator, impl_sysvar_get,
     incinerator, instruction, keccak, lamports, loader_instruction, loader_upgradeable_instruction,
     loader_v4, loader_v4_instruction, message, msg, native_token, nonce, program, program_error,
-    program_memory, program_option, program_pack, rent, sanitize, secp256k1_program,
+    program_memory, program_option, program_pack, rent, secp256k1_program,
     secp256k1_recover, serde_varint, serialize_utils, short_vec, slot_hashes, slot_history,
     stable_layout, stake, stake_history, syscalls, system_instruction, system_program, sysvar,
     unchecked_div_by_const, vote,
