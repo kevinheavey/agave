90c506055e6632e21de5e59ac0f1d5088a3eddd3
kevinheavey <heaveykevin2@gmail.com>
2024-07-03 10:19:36 +0400

    re-export sol_secp256k1_recover with deprecation warning

--- sdk/program/src/syscalls/definitions.rs
+++ sdk/program/src/syscalls/definitions.rs
@@ -1,5 +1,7 @@
 #[cfg(target_feature = "static-syscalls")]
 pub use solana_define_syscall::sys_hash;
+#[deprecated(since = "2.1.0", note = "Use `solana_secp256k1_recover::sol_secp256k1_recover` instead")]
+pub use solana_secp256k1_recover::sol_secp256k1_recover;
 use {
     crate::{
         instruction::{AccountMeta, ProcessedSiblingInstruction},
