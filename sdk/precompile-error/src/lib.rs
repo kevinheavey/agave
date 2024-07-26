2f50c6877c225c0cc7da687b8b6410f4dd5bc9d7
kevinheavey <heaveykevin2@gmail.com>
2024-07-26 15:37:51 +0400

    remove thiserror from precompile-error crate

--- sdk/precompile-error/src/lib.rs
+++ sdk/precompile-error/src/lib.rs
@@ -1,23 +1,32 @@
-use {
-    num_derive::{FromPrimitive, ToPrimitive},
-    solana_decode_error::DecodeError,
-    thiserror::Error
-};
+/// Precompile errors
+use {core::fmt, num_derive::{FromPrimitive, ToPrimitive}, solana_decode_error::DecodeError};
 
 /// Precompile errors
-#[derive(Error, Debug, Clone, PartialEq, Eq, FromPrimitive, ToPrimitive)]
+#[derive(Debug, Clone, PartialEq, Eq, FromPrimitive, ToPrimitive)]
 pub enum PrecompileError {
-    #[error("public key is not valid")]
     InvalidPublicKey,
-    #[error("id is not valid")]
     InvalidRecoveryId,
-    #[error("signature is not valid")]
     InvalidSignature,
-    #[error("offset not valid")]
     InvalidDataOffsets,
-    #[error("instruction is incorrect size")]
     InvalidInstructionDataSize,
 }
+
+impl std::error::Error for PrecompileError {}
+
+impl fmt::Display for PrecompileError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PrecompileError::InvalidPublicKey => f.write_str("public key is not valid"),
+            PrecompileError::InvalidRecoveryId => f.write_str("id is not valid"),
+            PrecompileError::InvalidSignature => f.write_str("signature is not valid"),
+            PrecompileError::InvalidDataOffsets => f.write_str("offset not valid"),
+            PrecompileError::InvalidInstructionDataSize => {
+                f.write_str("instruction is incorrect size")
+            }
+        }
+    }
+}
+
 impl<T> DecodeError<T> for PrecompileError {
     fn type_of() -> &'static str {
         "PrecompileError"
