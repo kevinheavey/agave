27ab79c41f0ca76ff7589eb011d5cf6affc3d023
kevinheavey <heaveykevin2@gmail.com>
2024-07-31 15:22:43 +0400

    fmt after rebase

--- sdk/program/src/lib.rs
+++ sdk/program/src/lib.rs
@@ -529,8 +529,6 @@ pub mod sysvar;
 pub mod vote;
 pub mod wasm;
 
-pub use solana_clock as clock;
-pub use solana_msg::msg;
 #[deprecated(since = "2.1.0", note = "Use `solana-program-memory` crate instead")]
 pub use solana_program_memory as program_memory;
 #[deprecated(since = "2.1.0", note = "Use `solana-sanitize` crate instead")]
@@ -541,6 +539,7 @@ pub use solana_secp256k1_recover as secp256k1_recover;
 pub use solana_short_vec as short_vec;
 #[cfg(target_arch = "wasm32")]
 pub use wasm_bindgen::prelude::wasm_bindgen;
+pub use {solana_clock as clock, solana_msg::msg};
 
 /// The [config native program][np].
 ///
