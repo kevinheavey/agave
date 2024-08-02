bf2a36db52038b35d4607c212141302432d26b92
kevinheavey <heaveykevin2@gmail.com>
2024-08-02 18:55:50 +0400

    update digests

--- sdk/program/src/message/versions/mod.rs
+++ sdk/program/src/message/versions/mod.rs
@@ -34,7 +34,7 @@ pub const MESSAGE_VERSION_PREFIX: u8 = 0x80;
 /// format.
 #[cfg_attr(
     feature = "frozen-abi",
-    frozen_abi(digest = "EjjHMjAnRrd86DuTgysFXRicMiAQv3vTvzRzcMJCjYfC"),
+    frozen_abi(digest = "E5AxBMRvFTnSBJyiAB9DGzeJqE7qeFLZMziS8kabu8Bg"),
     derive(AbiEnumVisitor, AbiExample)
 )]
 #[derive(Debug, PartialEq, Eq, Clone)]
