8656b85c720dca2bc7cceb15e89668fcbc3edbf7
kevinheavey <heaveykevin2@gmail.com>
2024-09-06 20:31:18 +0400

    update digests

--- sdk/src/transaction/mod.rs
+++ sdk/src/transaction/mod.rs
@@ -172,7 +172,7 @@ pub type Result<T> = result::Result<T, TransactionError>;
 #[cfg_attr(
     feature = "frozen-abi",
     derive(AbiExample),
-    frozen_abi(digest = "GNfV7vYLggBbde9n1xYKE8koExFLdr3yti7zDp7xShJR")
+    frozen_abi(digest = "686AAhRhjXpqKidmJEdHHcJCL9XxCxebu8Xmku9shp83")
 )]
 #[derive(Debug, PartialEq, Default, Eq, Clone, Serialize, Deserialize)]
 pub struct Transaction {
