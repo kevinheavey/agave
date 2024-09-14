9d751450bc90c0a0428508efbc80904c5d5e5638
kevinheavey <heaveykevin2@gmail.com>
2024-09-14 21:41:14 +0400

    remove thiserror

--- sdk/transaction-error/src/lib.rs
+++ sdk/transaction-error/src/lib.rs
@@ -5,175 +5,222 @@ use serde_derive::{Deserialize, Serialize};
 use solana_frozen_abi_macro::{AbiEnumVisitor, AbiExample};
 #[cfg(not(target_os = "solana"))]
 use solana_program::message::{AddressLoaderError, SanitizeMessageError};
-use {
-    solana_program::instruction::InstructionError, solana_sanitize::SanitizeError, thiserror::Error,
-};
+use {core::fmt, solana_program::instruction::InstructionError, solana_sanitize::SanitizeError};
 
 /// Reasons a transaction might be rejected.
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample, AbiEnumVisitor))]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[derive(Error, Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum TransactionError {
     /// An account is already being processed in another transaction in a way
     /// that does not support parallelism
-    #[error("Account in use")]
     AccountInUse,
 
     /// A `Pubkey` appears twice in the transaction's `account_keys`.  Instructions can reference
     /// `Pubkey`s more than once but the message must contain a list with no duplicate keys
-    #[error("Account loaded twice")]
     AccountLoadedTwice,
 
     /// Attempt to debit an account but found no record of a prior credit.
-    #[error("Attempt to debit an account but found no record of a prior credit.")]
     AccountNotFound,
 
     /// Attempt to load a program that does not exist
-    #[error("Attempt to load a program that does not exist")]
     ProgramAccountNotFound,
 
     /// The from `Pubkey` does not have sufficient balance to pay the fee to schedule the transaction
-    #[error("Insufficient funds for fee")]
     InsufficientFundsForFee,
 
     /// This account may not be used to pay transaction fees
-    #[error("This account may not be used to pay transaction fees")]
     InvalidAccountForFee,
 
     /// The bank has seen this transaction before. This can occur under normal operation
     /// when a UDP packet is duplicated, as a user error from a client not updating
     /// its `recent_blockhash`, or as a double-spend attack.
-    #[error("This transaction has already been processed")]
     AlreadyProcessed,
 
     /// The bank has not seen the given `recent_blockhash` or the transaction is too old and
     /// the `recent_blockhash` has been discarded.
-    #[error("Blockhash not found")]
     BlockhashNotFound,
 
     /// An error occurred while processing an instruction. The first element of the tuple
     /// indicates the instruction index in which the error occurred.
-    #[error("Error processing Instruction {0}: {1}")]
     InstructionError(u8, InstructionError),
 
     /// Loader call chain is too deep
-    #[error("Loader call chain is too deep")]
     CallChainTooDeep,
 
     /// Transaction requires a fee but has no signature present
-    #[error("Transaction requires a fee but has no signature present")]
     MissingSignatureForFee,
 
     /// Transaction contains an invalid account reference
-    #[error("Transaction contains an invalid account reference")]
     InvalidAccountIndex,
 
     /// Transaction did not pass signature verification
-    #[error("Transaction did not pass signature verification")]
     SignatureFailure,
 
     /// This program may not be used for executing instructions
-    #[error("This program may not be used for executing instructions")]
     InvalidProgramForExecution,
 
     /// Transaction failed to sanitize accounts offsets correctly
     /// implies that account locks are not taken for this TX, and should
     /// not be unlocked.
-    #[error("Transaction failed to sanitize accounts offsets correctly")]
     SanitizeFailure,
 
-    #[error("Transactions are currently disabled due to cluster maintenance")]
     ClusterMaintenance,
 
     /// Transaction processing left an account with an outstanding borrowed reference
-    #[error("Transaction processing left an account with an outstanding borrowed reference")]
     AccountBorrowOutstanding,
 
     /// Transaction would exceed max Block Cost Limit
-    #[error("Transaction would exceed max Block Cost Limit")]
     WouldExceedMaxBlockCostLimit,
 
     /// Transaction version is unsupported
-    #[error("Transaction version is unsupported")]
     UnsupportedVersion,
 
     /// Transaction loads a writable account that cannot be written
-    #[error("Transaction loads a writable account that cannot be written")]
     InvalidWritableAccount,
 
     /// Transaction would exceed max account limit within the block
-    #[error("Transaction would exceed max account limit within the block")]
     WouldExceedMaxAccountCostLimit,
 
     /// Transaction would exceed account data limit within the block
-    #[error("Transaction would exceed account data limit within the block")]
     WouldExceedAccountDataBlockLimit,
 
     /// Transaction locked too many accounts
-    #[error("Transaction locked too many accounts")]
     TooManyAccountLocks,
 
     /// Address lookup table not found
-    #[error("Transaction loads an address table account that doesn't exist")]
     AddressLookupTableNotFound,
 
     /// Attempted to lookup addresses from an account owned by the wrong program
-    #[error("Transaction loads an address table account with an invalid owner")]
     InvalidAddressLookupTableOwner,
 
     /// Attempted to lookup addresses from an invalid account
-    #[error("Transaction loads an address table account with invalid data")]
     InvalidAddressLookupTableData,
 
     /// Address table lookup uses an invalid index
-    #[error("Transaction address table lookup uses an invalid index")]
     InvalidAddressLookupTableIndex,
 
     /// Transaction leaves an account with a lower balance than rent-exempt minimum
-    #[error("Transaction leaves an account with a lower balance than rent-exempt minimum")]
     InvalidRentPayingAccount,
 
     /// Transaction would exceed max Vote Cost Limit
-    #[error("Transaction would exceed max Vote Cost Limit")]
     WouldExceedMaxVoteCostLimit,
 
     /// Transaction would exceed total account data limit
-    #[error("Transaction would exceed total account data limit")]
     WouldExceedAccountDataTotalLimit,
 
     /// Transaction contains a duplicate instruction that is not allowed
-    #[error("Transaction contains a duplicate instruction ({0}) that is not allowed")]
     DuplicateInstruction(u8),
 
     /// Transaction results in an account with insufficient funds for rent
-    #[error(
-        "Transaction results in an account ({account_index}) with insufficient funds for rent"
-    )]
-    InsufficientFundsForRent { account_index: u8 },
+    InsufficientFundsForRent {
+        account_index: u8,
+    },
 
     /// Transaction exceeded max loaded accounts data size cap
-    #[error("Transaction exceeded max loaded accounts data size cap")]
     MaxLoadedAccountsDataSizeExceeded,
 
     /// LoadedAccountsDataSizeLimit set for transaction must be greater than 0.
-    #[error("LoadedAccountsDataSizeLimit set for transaction must be greater than 0.")]
     InvalidLoadedAccountsDataSizeLimit,
 
     /// Sanitized transaction differed before/after feature activiation. Needs to be resanitized.
-    #[error("ResanitizationNeeded")]
     ResanitizationNeeded,
 
     /// Program execution is temporarily restricted on an account.
-    #[error("Execution of the program referenced by account at index {account_index} is temporarily restricted.")]
-    ProgramExecutionTemporarilyRestricted { account_index: u8 },
+    ProgramExecutionTemporarilyRestricted {
+        account_index: u8,
+    },
 
     /// The total balance before the transaction does not equal the total balance after the transaction
-    #[error("Sum of account balances before and after transaction do not match")]
     UnbalancedTransaction,
 
     /// Program cache hit max limit.
-    #[error("Program cache hit max limit")]
     ProgramCacheHitMaxLimit,
+}
+
+impl std::error::Error for TransactionError {}
+
+impl fmt::Display for TransactionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::AccountInUse
+             => f.write_str("Account in use"),
+            Self::AccountLoadedTwice
+             => f.write_str("Account loaded twice"),
+            Self::AccountNotFound
+             => f.write_str("Attempt to debit an account but found no record of a prior credit."),
+            Self::ProgramAccountNotFound
+             => f.write_str("Attempt to load a program that does not exist"),
+            Self::InsufficientFundsForFee
+             => f.write_str("Insufficient funds for fee"),
+            Self::InvalidAccountForFee
+             => f.write_str("This account may not be used to pay transaction fees"),
+            Self::AlreadyProcessed
+             => f.write_str("This transaction has already been processed"),
+            Self::BlockhashNotFound
+             => f.write_str("Blockhash not found"),
+            Self::InstructionError(idx, err) =>  write!(f, "Error processing Instruction {idx}: {err}"),
+            Self::CallChainTooDeep
+             => f.write_str("Loader call chain is too deep"),
+            Self::MissingSignatureForFee
+             => f.write_str("Transaction requires a fee but has no signature present"),
+            Self::InvalidAccountIndex
+             => f.write_str("Transaction contains an invalid account reference"),
+            Self::SignatureFailure
+             => f.write_str("Transaction did not pass signature verification"),
+            Self::InvalidProgramForExecution
+             => f.write_str("This program may not be used for executing instructions"),
+            Self::SanitizeFailure
+             => f.write_str("Transaction failed to sanitize accounts offsets correctly"),
+            Self::ClusterMaintenance
+             => f.write_str("Transactions are currently disabled due to cluster maintenance"),
+            Self::AccountBorrowOutstanding
+             => f.write_str("Transaction processing left an account with an outstanding borrowed reference"),
+            Self::WouldExceedMaxBlockCostLimit
+             => f.write_str("Transaction would exceed max Block Cost Limit"),
+            Self::UnsupportedVersion
+             => f.write_str("Transaction version is unsupported"),
+            Self::InvalidWritableAccount
+             => f.write_str("Transaction loads a writable account that cannot be written"),
+            Self::WouldExceedMaxAccountCostLimit
+             => f.write_str("Transaction would exceed max account limit within the block"),
+            Self::WouldExceedAccountDataBlockLimit
+             => f.write_str("Transaction would exceed account data limit within the block"),
+            Self::TooManyAccountLocks
+             => f.write_str("Transaction locked too many accounts"),
+            Self::AddressLookupTableNotFound
+             => f.write_str("Transaction loads an address table account that doesn't exist"),
+            Self::InvalidAddressLookupTableOwner
+             => f.write_str("Transaction loads an address table account with an invalid owner"),
+            Self::InvalidAddressLookupTableData
+             => f.write_str("Transaction loads an address table account with invalid data"),
+            Self::InvalidAddressLookupTableIndex
+             => f.write_str("Transaction address table lookup uses an invalid index"),
+            Self::InvalidRentPayingAccount
+             => f.write_str("Transaction leaves an account with a lower balance than rent-exempt minimum"),
+            Self::WouldExceedMaxVoteCostLimit
+             => f.write_str("Transaction would exceed max Vote Cost Limit"),
+            Self::WouldExceedAccountDataTotalLimit
+             => f.write_str("Transaction would exceed total account data limit"),
+            Self::DuplicateInstruction(idx) =>  write!(f, "Transaction contains a duplicate instruction ({idx}) that is not allowed"),
+            Self::InsufficientFundsForRent {
+                account_index
+            } =>  write!(f,"Transaction results in an account ({account_index}) with insufficient funds for rent"),
+            Self::MaxLoadedAccountsDataSizeExceeded
+             => f.write_str("Transaction exceeded max loaded accounts data size cap"),
+            Self::InvalidLoadedAccountsDataSizeLimit
+             => f.write_str("LoadedAccountsDataSizeLimit set for transaction must be greater than 0."),
+            Self::ResanitizationNeeded
+             => f.write_str("ResanitizationNeeded"),
+            Self::ProgramExecutionTemporarilyRestricted {
+                account_index
+            } =>  write!(f,"Execution of the program referenced by account at index {account_index} is temporarily restricted."),
+            Self::UnbalancedTransaction
+             => f.write_str("Sum of account balances before and after transaction do not match"),
+            Self::ProgramCacheHitMaxLimit
+             => f.write_str("Program cache hit max limit"),
+        }
+    }
 }
 
 impl From<SanitizeError> for TransactionError {
