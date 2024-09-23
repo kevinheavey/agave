a8b6c3886e0a6f2a013544528e216f49c54ab255
kevinheavey <heaveykevin2@gmail.com>
2024-09-23 17:09:08 +0400

    extract presigner crate

--- sdk/src/signer/mod.rs
+++ sdk/src/signer/mod.rs
@@ -1,9 +1,9 @@
 #![cfg(feature = "full")]
 pub use {
+    solana_presigner as presigner,
     solana_seed_derivable::SeedDerivable,
     solana_signer::{
-        null_signer, presigner, signers, unique_signers, EncodableKey, EncodableKeypair, Signer,
-        SignerError,
+        null_signer, signers, unique_signers, EncodableKey, EncodableKeypair, Signer, SignerError,
     },
 };
 pub mod keypair;
