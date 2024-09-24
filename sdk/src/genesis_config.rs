0d28e5d9fefe7ea50071c549ee8e922fc99eb91d
kevinheavey <heaveykevin2@gmail.com>
2024-09-25 03:02:56 +0400

    update digest

--- sdk/src/genesis_config.rs
+++ sdk/src/genesis_config.rs
@@ -87,7 +87,7 @@ impl FromStr for ClusterType {
 #[cfg_attr(
     feature = "frozen-abi",
     derive(AbiExample),
-    frozen_abi(digest = "2eGYc5mpKqDsS8sZfNS4mVq4qPptXYa9hSid2Hpv4DkQ")
+    frozen_abi(digest = "3F7oyDqLE2tNKkgyasY7duwDBQM7qvVjXvCtAPTs8rqN")
 )]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct GenesisConfig {
