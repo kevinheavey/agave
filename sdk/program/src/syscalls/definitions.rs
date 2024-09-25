d29e0e94458a7c2cb1f1ed3e64ca38fa6fd677ee
kevinheavey <heaveykevin2@gmail.com>
2024-09-25 15:33:03 +0400

    extract cpi crate

--- sdk/program/src/syscalls/definitions.rs
+++ sdk/program/src/syscalls/definitions.rs
@@ -1,3 +1,5 @@
+#[deprecated(since = "2.1.0", note = "Use `solana_cpi::syscalls` instead")]
+pub use solana_cpi::syscalls::{sol_get_return_data, sol_invoke_signed_rust, sol_set_return_data};
 #[cfg(target_feature = "static-syscalls")]
 pub use solana_define_syscall::sys_hash;
 #[deprecated(since = "2.1.0", note = "Use `solana_instruction::syscalls` instead")]
@@ -26,9 +28,6 @@ use {crate::pubkey::Pubkey, solana_define_syscall::define_syscall};
 define_syscall!(fn sol_keccak256(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_blake3(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_invoke_signed_c(instruction_addr: *const u8, account_infos_addr: *const u8, account_infos_len: u64, signers_seeds_addr: *const u8, signers_seeds_len: u64) -> u64);
-define_syscall!(fn sol_invoke_signed_rust(instruction_addr: *const u8, account_infos_addr: *const u8, account_infos_len: u64, signers_seeds_addr: *const u8, signers_seeds_len: u64) -> u64);
-define_syscall!(fn sol_set_return_data(data: *const u8, length: u64));
-define_syscall!(fn sol_get_return_data(data: *mut u8, length: u64, program_id: *mut Pubkey) -> u64);
 define_syscall!(fn sol_curve_validate_point(curve_id: u64, point_addr: *const u8, result: *mut u8) -> u64);
 define_syscall!(fn sol_curve_group_op(curve_id: u64, group_op: u64, left_input_addr: *const u8, right_input_addr: *const u8, result_point_addr: *mut u8) -> u64);
 define_syscall!(fn sol_curve_multiscalar_mul(curve_id: u64, scalars_addr: *const u8, points_addr: *const u8, points_len: u64, result_point_addr: *mut u8) -> u64);
