2c93f2662dd7258140f1668b5750444979044413
kevinheavey <heaveykevin2@gmail.com>
2024-09-26 13:25:09 +0400

    unused import

--- sdk/program/src/syscalls/definitions.rs
+++ sdk/program/src/syscalls/definitions.rs
@@ -24,7 +24,7 @@ pub use solana_pubkey::syscalls::{
 pub use solana_secp256k1_recover::sol_secp256k1_recover;
 #[deprecated(since = "2.1.0", note = "Use solana_sha256_hasher::sol_sha256 instead")]
 pub use solana_sha256_hasher::sol_sha256;
-use {crate::pubkey::Pubkey, solana_define_syscall::define_syscall};
+use solana_define_syscall::define_syscall;
 define_syscall!(fn sol_keccak256(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_blake3(vals: *const u8, val_len: u64, hash_result: *mut u8) -> u64);
 define_syscall!(fn sol_invoke_signed_c(instruction_addr: *const u8, account_infos_addr: *const u8, account_infos_len: u64, signers_seeds_addr: *const u8, signers_seeds_len: u64) -> u64);
