c3f2fd2c47573ae5821c4803797c6a95b3a24112
kevinheavey <heaveykevin2@gmail.com>
2024-10-15 20:06:23 +0400

    remove superfluous import

--- sdk/account/src/state_traits.rs
+++ sdk/account/src/state_traits.rs
@@ -64,11 +64,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use {
-        super::*,
-        solana_account::{state_traits::StateMut, AccountSharedData},
-        solana_pubkey::Pubkey,
-    };
+    use {super::*, solana_pubkey::Pubkey};
 
     #[test]
     fn test_account_state() {
