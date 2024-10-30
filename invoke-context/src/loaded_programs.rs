94f9aa5a0fb3c62524b1446f4d89fe1aa0759bde
kevinheavey <heaveykevin2@gmail.com>
2024-10-30 14:03:56 +0400

    fix macro invocation

--- invoke-context/src/loaded_programs.rs
+++ invoke-context/src/loaded_programs.rs
@@ -296,7 +296,7 @@ impl LoadProgramMetrics {
         saturating_add_assign!(timings.create_executor_load_elf_us, self.load_elf_us);
         saturating_add_assign!(timings.create_executor_verify_code_us, self.verify_code_us);
         saturating_add_assign!(timings.create_executor_jit_compile_us, self.jit_compile_us);
-        datapoint_trace!(
+        solana_metrics::datapoint_trace!(
             "create_executor_trace",
             ("program_id", self.program_id, String),
             ("register_syscalls_us", self.register_syscalls_us, i64),
