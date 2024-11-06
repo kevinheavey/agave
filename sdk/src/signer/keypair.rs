06426cc53236039eca9aac6569d6de62c1a569d2
Jon C <me@jonc.dev>
2024-11-06 02:28:45 +0100

    Add deprecation notices

--- sdk/src/signer/keypair.rs
+++ sdk/src/signer/keypair.rs
@@ -1,4 +1,8 @@
-pub use {
-    solana_keypair::*, solana_seed_derivable::keypair_from_seed_and_derivation_path,
-    solana_seed_phrase::generate_seed_from_seed_phrase_and_passphrase, solana_signer::*,
-};
+#[deprecated(since = "2.2.0", note = "Use `solana-keypair` crate instead")]
+pub use solana_keypair::*;
+#[deprecated(since = "2.2.0", note = "Use `solana-seed-derivable` crate instead")]
+pub use solana_seed_derivable::keypair_from_seed_and_derivation_path;
+#[deprecated(since = "2.2.0", note = "Use `solana-seed-phrase` crate instead")]
+pub use solana_seed_phrase::generate_seed_from_seed_phrase_and_passphrase;
+#[deprecated(since = "2.2.0", note = "Use `solana-signer` crate instead")]
+pub use solana_signer::*;
--- sdk/src/signer/mod.rs
+++ sdk/src/signer/mod.rs
@@ -1,9 +1,10 @@
 #![cfg(feature = "full")]
-pub use {
-    solana_presigner as presigner,
-    solana_seed_derivable::SeedDerivable,
-    solana_signer::{
-        null_signer, signers, unique_signers, EncodableKey, EncodableKeypair, Signer, SignerError,
-    },
+#[deprecated(since = "2.2.0", note = "Use `solana-presigner` crate instead")]
+pub use solana_presigner as presigner;
+#[deprecated(since = "2.2.0", note = "Use `solana-seed-derivable` crate instead")]
+pub use solana_seed_derivable::SeedDerivable;
+#[deprecated(since = "2.2.0", note = "Use `solana-signer` crate instead")]
+pub use solana_signer::{
+    null_signer, signers, unique_signers, EncodableKey, EncodableKeypair, Signer, SignerError,
 };
 pub mod keypair;
