83a39620fb4f86fd33b12e090febb92c85f4c3c0
Kevin Heavey <heaveykevin2@gmail.com>
2024-11-19 11:55:49 +0400

    Apply suggestions
    Co-authored-by: Jon C <me@jonc.dev>

--- sdk/program/src/sysvar.rs
+++ sdk/program/src/sysvar.rs
@@ -1,5 +1,6 @@
 #[deprecated(since = "2.1.0", note = "Use `solana-sysvar-id` crate instead")]
 pub use solana_sysvar_id::{declare_deprecated_sysvar_id, declare_sysvar_id, SysvarId};
+#[deprecated(since = "2.2.0", note = "Use `solana-sysvar` crate instead")]
 #[allow(deprecated)]
 pub use {
     solana_sdk_ids::sysvar::{check_id, id, ID},
