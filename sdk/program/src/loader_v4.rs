e572c2682b56756fe2dafa6d71df1ffefca18b22
kevinheavey <heaveykevin2@gmail.com>
2025-01-16 02:54:04 +0400

    missing re-exports

--- sdk/program/src/loader_v4.rs
+++ sdk/program/src/loader_v4.rs
@@ -1,5 +1,11 @@
 #[deprecated(since = "2.2.0", note = "Use solana-loader-v4-interface instead")]
 pub use solana_loader_v4_interface::{
+    instruction::{
+        create_buffer, deploy, deploy_from_source, finalize, is_deploy_instruction,
+        is_finalize_instruction, is_retract_instruction, is_transfer_authority_instruction,
+        is_truncate_instruction, is_write_instruction, retract, transfer_authority, truncate,
+        truncate_uninitialized, write,
+    },
     state::{LoaderV4State, LoaderV4Status},
     DEPLOYMENT_COOLDOWN_IN_SLOTS,
 };
