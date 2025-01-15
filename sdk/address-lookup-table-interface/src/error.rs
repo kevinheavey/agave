d50f90608996c1b4214c024239e005dfe7c9badd
kevinheavey <heaveykevin2@gmail.com>
2025-01-15 21:23:14 +0400

    remove thiserror from address-lookup-table-interface

--- sdk/address-lookup-table-interface/src/error.rs
+++ sdk/address-lookup-table-interface/src/error.rs
@@ -1,20 +1,31 @@
-use thiserror::Error;
+use core::fmt;
 
-#[derive(Debug, Error, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum AddressLookupError {
     /// Attempted to lookup addresses from a table that does not exist
-    #[error("Attempted to lookup addresses from a table that does not exist")]
     LookupTableAccountNotFound,
 
     /// Attempted to lookup addresses from an account owned by the wrong program
-    #[error("Attempted to lookup addresses from an account owned by the wrong program")]
     InvalidAccountOwner,
 
     /// Attempted to lookup addresses from an invalid account
-    #[error("Attempted to lookup addresses from an invalid account")]
     InvalidAccountData,
 
     /// Address lookup contains an invalid index
-    #[error("Address lookup contains an invalid index")]
     InvalidLookupIndex,
+}
+
+impl fmt::Display for AddressLookupError {
+    fn fmt(&self, f: &mut ::core::fmt::Formatter) -> fmt::Result {
+        f.write_str(match self {
+            Self::LookupTableAccountNotFound => {
+                "Attempted to lookup addresses from a table that does not exist"
+            }
+            Self::InvalidAccountOwner => {
+                "Attempted to lookup addresses from an account owned by the wrong program"
+            }
+            Self::InvalidAccountData => "Attempted to lookup addresses from an invalid account",
+            Self::InvalidLookupIndex => "Address lookup contains an invalid index",
+        })
+    }
 }
