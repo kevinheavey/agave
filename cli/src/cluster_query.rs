b54103dbfb504f41e60edeedd3146a2850574a1a
kevinheavey <heaveykevin2@gmail.com>
2025-01-29 13:57:24 +0400

    cli: demote solana-sdk to dev deps

--- cli/src/cluster_query.rs
+++ cli/src/cluster_query.rs
@@ -50,7 +50,6 @@ use {
         request::DELINQUENT_VALIDATOR_SLOT_DISTANCE,
         response::{RpcPerfSample, RpcPrioritizationFee, SlotInfo},
     },
-    solana_sdk::rpc_port::DEFAULT_RPC_PORT_STR,
     solana_sdk_ids::sysvar::{self, stake_history},
     solana_signature::Signature,
     solana_slot_history::{self as slot_history, SlotHistory},
@@ -76,6 +75,14 @@ use {
     },
     thiserror::Error,
 };
+
+const DEFAULT_RPC_PORT_STR: &str = "8899";
+
+#[cfg(test)]
+static_assertions::const_assert_eq!(
+    DEFAULT_RPC_PORT_STR,
+    solana_sdk::rpc_port::DEFAULT_RPC_PORT_STR
+);
 
 pub trait ClusterQuerySubCommands {
     fn cluster_query_subcommands(self) -> Self;
